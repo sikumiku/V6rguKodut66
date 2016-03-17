e6fa122bf193451bfe412b4a475cd51b75afd902
Sigrid <sigridaasma@gmail.com>
2016-03-17 16:21:12 +0200

    5. n2dal

---
+++ abakus.js
@@ -0,0 +1,17 @@
+window.onload = function() {
+
+	var p2rlid = document.getElementsByClassName('bead');
+
+	function liigutaP2rl(p2rl) {
+
+	    if (p2rl.target.style.cssFloat == "left") {
+	    	p2rl.target.style.cssFloat = "right"; 
+	    } else {
+	    	p2rl.target.style.cssFloat = "left"; 
+	    }
+	}
+
+	for (var i = 0; i < p2rlid.length; i++) {
+		p2rlid[i].onclick = liigutaP2rl;
+	}
+}
